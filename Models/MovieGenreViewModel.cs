cdba0275c7e75ee5a9ed28f45763e7c85cd9b64d
Iffat Binte Sikder <isikder@my.centennialcollege.ca>
2024-12-02 22:47:01 -0500

    Initial commit

---
+++ Models/MovieGenreViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MvcMovie.Models
+{
+	public class MovieGenreViewModel
+	{
+        internal List<Movie> Movies;
+
+        public string SearchString { get; set; }
+        public string MovieGenre { get; set; }
+
+        public SelectList Genres { get; internal set; }
+    }
+}
+
